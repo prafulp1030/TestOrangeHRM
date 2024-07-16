8b08142f77288b10abd6c36548051399058677e9
prafpawar11 <prafpawar11@gmail.com>
2024-07-16 20:29:20 +0530

    login Tc by SurajTester

--- src/main/java/Tutorial9/EndToEndTC.java
+++ src/main/java/Tutorial9/EndToEndTC.java
@@ -2,6 +2,7 @@ package Tutorial9;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -15,6 +16,17 @@ public class EndToEndTC {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+	@Test(priority = 1)
+	public void validateLoginFunctionality() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+		Thread.sleep(2000);
+
 	}
 
 }
